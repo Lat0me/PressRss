4b9a4081bf6833ff851a5a84d8bfb9e1f861693e
Antoine Cordier <antoine.cordier@display-interactive.com>
2017-09-11 10:39:32 +0200

    Add save article

--- function/fluxRss/rss.php
+++ function/fluxRss/rss.php
@@ -1,23 +1,25 @@
 <?php
 
 
-function rssJournal ($urlJournal)
-{
-    $url = $urlJournal;
-    $rss = simplexml_load_file($url);
-    foreach ($rss->channel->item as $item) {
-        $datetime = date_create($item->pubDate);
-        $date = date_format($datetime, 'd M Y, H\hi');
-        echo "<div class=\"col-sm-5\">
-                    <div class='card'>
-                        <div class='card-block'>
-                            <h3 class='card-title'>". ($item->title). "</h3>
-                            <p class='card-text'>". ($item->description) ."</p>
-                            ". $date ."
-                            <a href='. $item->link .' class='btn btn-primary'>site de l'article</a>
-                        </div>
+function afficherArticle($title, $link, $date, $id){
+    echo "
+            <div class=\"col-sm-3\">
+                <div class='card card-block h-100 justify-content-center'>
+                    <div class='card-block'>
+                        <h5 class='card-title'>". $title . "</h5>
+                        <form method=\"post\">
+                        <a href='$link' class='btn btn-primary'>Consultez l'article</a>
+                        <button name='$id' class=\"btn btn-success\">
+                                <i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i>
+                            </button><br>
+                        ". $date ."
+                        </form>
                     </div>
-                </div>";
+                </div>
+            </div>
+            ";
+    if(isset($_POST[$id])){
+        echo $id;
     }
 }
 
@@ -29,21 +31,22 @@ function rssArticle ($urlJournal)
     foreach ($rss->channel->item as $item) {
         $datetime = date_create($item->pubDate);
         $date = date_format($datetime, 'd M Y, H\hi');
+        $title = $item->title;
+        $link = $item->link;
+        $id = rand();
         $i++;
-
-        echo "
-            <div class=\"col-sm-3\">
-                <div class='card card-block h-100 justify-content-center'>
-                    <div class='card-block'>
-                        <h5 class='card-title'>".($item->title). "</h5>
-                        <a href='$item->link' class='btn btn-primary'>Consultez l'article</a>
-                        ". $date ."
-                    </div>
-                </div>
-            </div>
-            ";
-
-
+        afficherArticle($title, $link, $date, $id);
     }
+}
+
+function parseMysql ($title, $link) {
+    $titleSql = mysqli_real_escape_string($title);
+    $linkSql = mysqli_real_escape_string($link);
+
+}
+
+
+function saveArticle (){
+    echo "<button type=\"button\" class=\"btn btn-primary\"><i class=\"fa fa-floppy-o\" aria-hidden=\"true\"></i></button>";
 }
 
